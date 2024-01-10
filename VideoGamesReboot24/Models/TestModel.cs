05141eb5ff8771160e8ded8fa16bbf0f7e526dde
BluNightCrawler <jeremyfrance123@hotmail.com>
2024-01-10 15:41:45 -0800

    Create and Thanks Page With the model

--- VideoGamesReboot24/Models/TestModel.cs
+++ VideoGamesReboot24/Models/TestModel.cs
@@ -7,18 +7,18 @@ namespace VideoGamesReboot24.Models
     {
         public long ProductID { get; set; }
         [Required(ErrorMessage = "Please enter a Title")]
-        public string ProductName { get; set; }
+        public string ProductName { get; set;}
         [Required(ErrorMessage = "Please enter a Description")]
-        public string Description { get; set; }
+        public string Description { get; set;}
 
         [Required(ErrorMessage = "Please enter a Price")]
         [Column(TypeName = "decimal(8,2")]
-        public double Price { get; set; }
+        public double Price { get; set;}
         [Required(ErrorMessage = "Please enter a Genre")]
-        public string Category { get; set; }
+        public string Category { get; set;}
 
         [Required(ErrorMessage = "Please enter a System")]
-        public string System { get; set; }
+        public string System { get; set;}
         public bool Active { get; set; }
     }
 }

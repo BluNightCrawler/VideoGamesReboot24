ac6f0fe8b66d828b944bcf1fbf52d4385251d4a7
Michael Splaver <michaelsplaver@gmail.com>
2024-03-01 04:24:36 -0600

    LoginPartial Abstraction + working profile image

---
+++ VideoGamesReboot24/Components/LoginPartialViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VideoGamesReboot24.Models;
+
+namespace VideoGamesReboot24.Components
+{
+    public class LoginPartialViewComponent : ViewComponent
+    {
+        UserManager<AppUser> userManager;
+
+        public LoginPartialViewComponent(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            AppUser appUser = await userManager.GetUserAsync(HttpContext.User);
+            return View(appUser);
+        }
+    }
+}
